93c072b1384af0e5cde6f497f1f129dda6344f44
jefferson alves <https://github.com/jeffersonf-alves/aprendendo_reactjs-.git>
2021-01-11 18:40:41 -0300

    Primeis aulas

---
+++ importando_react_primeirosPassos/script.js
@@ -0,0 +1,10 @@
+const root = document.querySelector('#like_button_container');
+
+
+const t1 = React.createElement('h1', {id:"primeiro elemento"});
+
+
+const headline = React.createElement('div', null, t1);
+
+
+ReactDOM.render(headline, root);
